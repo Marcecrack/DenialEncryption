31266572a040c84df89c308272dc6bbe5aa50e3b
Marcelo1038 <chelo1150@gmail.com>
2021-04-05 19:46:28 -0500

    cambio

--- DenialEncryption/Form3.cs
+++ DenialEncryption/Form3.cs
@@ -22,13 +22,23 @@ namespace DenialEncryption
             using StreamReader r = new StreamReader(string.Concat(System.Environment.CurrentDirectory, "\\messages.txt"));
             string json = r.ReadToEnd();
             var temp = JObject.Parse(json);
-
+            string test;
             var message = GetMessages(temp);
-            var test = message.ListMessages.Where(m => m.Key == textBox2.Text).FirstOrDefault().Text;
-            if (!string.IsNullOrEmpty(test))
+
+            if (message.Text != null)
             {
-                var result = EncryptHelper.Decrypt(test, textBox2.Text);
-                MessageBox.Show(result);
+                var exist = message.ListMessages.Exists(m => m.Key == textBox2.Text);
+                if (!string.IsNullOrEmpty(temp.ToString()))
+                {
+                    if (message.ListMessages.Count != 0)
+                    {
+                        test = message.ListMessages.FirstOrDefault(m => m.Key == textBox2.Text).Text;
+                        var result = EncryptHelper.Decrypt(test, textBox2.Text);
+                        MessageBox.Show(result);
+                    }
+                    else
+                        MessageBox.Show("Wow...you were tricked");
+                }
             }
             else
                 MessageBox.Show("Wow...you were tricked");
@@ -43,11 +53,16 @@ namespace DenialEncryption
                 {
                     var test = mes.Value.ToList();
                     message.ListMessages = new List<Message>();
+                    
                     foreach (var prueba in test)
                     {
-                        message.ListMessages.Add(JsonConvert.DeserializeObject<Message>(prueba.ToString()));
+                        var jsonA = JsonConvert.DeserializeObject<Message>(prueba.ToString());
+                        if (jsonA.Key != textBox2.Text)
+                            continue;
+                        message.ListMessages.Add(jsonA);
+                        actual = false;
+                        break;
                     }
-                    actual = false;
                 }
                 if (string.Equals(mes.Value.ToString(), textBox1.Text))
                 {
